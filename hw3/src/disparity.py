378930012e9fe1dda61acbe675544a1a50c33010
Douglas Chan <dtchan@eng.ucsd.edu>
2016-01-24 16:50:18 -0800

    preliminary disparity map code

--- hw3/src/disparity.py
+++ hw3/src/disparity.py
@@ -7,26 +7,34 @@ import sys
 import numpy as np
 import rospy
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-pub = rospy.Publisher('disparity', Float32, queue_size=10)
+pub = rospy.Publisher('disparity', Image, queue_size=10)
 rospy.init_node('disparity_node', anonymous=True)
+
 leftImg = 0
 rightImg = 0
-
+bridge = CvBridge()
+rightReady = False
+leftReady = False
 
 def left_image_callback(msg):
-    global leftImg
-    leftImg = msg.data
+    global leftImg, leftReady
+    leftReady = True
+    leftImg = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
 def right_image_callback(msg):
-    global rightImg
-    rightImg = msg.data
+    global rightImg, rightReady
+    rightReady = True
+    rightImg = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
 
+rospy.Subscriber("left_cam/image_raw", Image, left_image_callback)
+rospy.Subscriber("right_cam/image_raw", Image, right_image_callback)
 
 def getDisparity(imgLeft, imgRight, method="BM"):
 
-    gray_left = cv2.cvtColor(imgLeft, cv.CV_BGR2GRAY)
-    gray_right = cv2.cvtColor(imgRight, cv.CV_BGR2GRAY)
+    gray_left = cv2.cvtColor(imgLeft, cv.CV_BGR2HSV)
+    gray_right = cv2.cvtColor(imgRight, cv.CV_BGR2HSV)
     print gray_left.shape
     c, r = gray_left.shape
     if method == "BM":
@@ -67,11 +75,14 @@ def getDisparity(imgLeft, imgRight, method="BM"):
         disparity_visual = cv2.normalize(disparity, alpha=0, beta=255, norm_type=cv2.cv.CV_MINMAX, dtype=cv2.cv.CV_8U)
 
     return disparity_visual
-disparity_msg = Image()
+
 while True:
-    if leftImg == 0 or rightImg == 0:
+    # rospy.spinOnce()
+    if (not leftReady) or (not rightReady):
         continue
-    disparity_msg.data = getDisparity(leftImg, rightImg, "BM")
+    disparity = getDisparity(leftImg, rightImg, "BM")
+    disparity_msg = bridge.cv2_to_imgmsg(disparity, "8UC1")
+    
     pub.publish(disparity_msg)
 
 # imgLeft = cv2.imread(sys.argv[1])
