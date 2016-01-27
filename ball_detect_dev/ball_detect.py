33ddc4e3e00ef276bad690b8c4877eb5ef901cd3
Yixing Lao <linuxthink@gmail.com>
2016-01-27 01:16:00 -0800

    draft of big refactor ball detect

--- ball_detect_dev/ball_detect.py
+++ ball_detect_dev/ball_detect.py
@@ -2,63 +2,47 @@ import numpy as np
 import cv2
 
 camera = cv2.VideoCapture(0)
-
+hsv_lows = (0, 153, 72)
+hsv_highs = (16, 218, 146)
 
 while(True):
-    # camerature frame-by-frame
-    ret, frame0 = camera.read()
-    # Resize the resolution of image
-    frame = cv2.resize(frame0, (160, 120), interpolation=cv2.INTER_CUBIC)
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
+    # read frame
+    im_bgr = camera.read()
 
-    # find orange
-    for y in range(1, 160):
-        for x in range(1, 120):
-            if h[x, y] > 3 and h[x, y] < 8 and s[x, y] > 146 and s[x, y] < 255 and v[x, y] > 120 and v[x, y] < 255:
-                h[x, y] = h[x, y]
-            else:
-                h[x, y] = 255
-    ret, thresh = cv2.threshold(h, 60, 255, 0)
+    # resize
+    # im_bgr = cv2.resize(im_bgr, (160, 120), interpolation=cv2.INTER_CUBIC)
+
+    # mask by threshold
+    im_mask = cv2.inRange(im_hsv, hsv_lows, hsv_highs)
     # erode
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
-    eroded = cv2.erode(thresh, kernel)
+    im_mask = cv2.erode(im_mask, None, iterations=2)
     # dilate
-    dilated = cv2.dilate(eroded, kernel)
-    # find contour
-    contours, hierarchy = cv2.findContours(dilated, 1, 2)
+    im_mask = cv2.dilate(im_mask, None, iterations=2)
 
+    # find contours
+    _, contours, _ = cv2.findContours(im_mask, cv2.RETR_EXTERNAL,
+                                      cv2.CHAIN_APPROX_SIMPLE)
+
+    # get centers and radiuses
     centers = []
     radiuses = []
 
-    for cnt in contours:
-        # Calculte the center of the ball
-        (center_x, center_y), radius = cv2.minEnclosingCircle(cnt)
+    for countour in contours:
+        center, radius = cv2.minEnclosingCircle(countour)
+        centers.append((int(center[0]), int(center[1])))  # a tuple
+        radiuses.append(int(radius))  # an int
 
-        center_x = center_x * 4
-        center_y = center_y * 4
-        radius = radius * 4
-
-        center = (int(center_x), int(center_y))
-        radius = int(radius)
-
-        centers.append(center)
-        radiuses.append(radius)
-
+    # plot center and radius
     for center, radius in zip(centers, radiuses):
-        # Plot the position of the ball
         if radius > 2:
-            cv2.circle(frame0, center, radius, (0, 255, 0), 2)
-            print center_x
-            print center_y
+            cv2.circle(im_bgr, center, radius, (0, 255, 0), 2)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame0)
+    # display the resulting frame
+    cv2.imshow('frame', im_bgr)
     key = cv2.waitKey(10)
     if key == 27:
         break
 
-# When everything done, release the camerature
+# when everything done, release the camera
 camera.release()
 cv2.destroyAllWindows()
