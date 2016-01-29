e10468635036f4fcaf9b14652aea117f6def0cdd
Yixing Lao <linuxthink@gmail.com>
2016-01-29 15:18:59 -0800

    style changes

--- ball_detect/src/ros_ball_detect_utils.py
+++ ball_detect/src/ros_ball_detect_utils.py
@@ -20,12 +20,15 @@ bridge = CvBridge()
 rightReady = False
 leftReady = False
 
+
 def left_image_callback(msg):
     global leftImg, leftReady
     # print "left"
     leftReady = True
     leftImg = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
     # ROS_ERROR("LEFT ")
+
+
 def right_image_callback(msg):
     global rightImg, rightReady
     # print "right"
@@ -35,24 +38,25 @@ def right_image_callback(msg):
 rospy.Subscriber("stereo/right/image_rect_color", Image, right_image_callback)
 rospy.Subscriber("stereo/left/image_rect_color", Image, left_image_callback)
 
+
 def calculate_depth(center1, center2, radius, radius2):
     center_x = center1[0]
     center_y = center1[1]
     center_x2 = center2[0]
     center_y2 = center2[1]
 
-    f=10
-    T=8
-    k=1000
+    f = 10
+    T = 8
+    k = 1000
     if center_y2 != center_y:
-        depth=f*T*k/(abs(center_y2-center_y)*radius)
+        depth = f * T * k / (abs(center_y2 - center_y) * radius)
         if True:
-            #print depth
+            # print depth
             # print x and z
-            kx=50
-            ky=0.0005
-            x=(center_x-center_y)*kx/depth
-            z=(240-center_y)*ky*depth
+            kx = 50
+            ky = 0.0005
+            x = (center_x - center_y) * kx / depth
+            z = (240 - center_y) * ky * depth
             # print x
             # print z
 
@@ -60,11 +64,11 @@ def calculate_depth(center1, center2, radius, radius2):
     # center2 = (int(center_x2),int(center_y2))
     # radius = int(radius)
     # radius2 = int(radius2)
-    # # Plot the position of the ball
+    # Plot the position of the ball
     # if radius>1 and radius2>1:
-    #     # cv2.circle(leftImg,center,radius,(0,255,0),2)
-    #     # cv2.circle(rightImg,center2,radius2,(0,255,0),2)
-    #     #calculate depth
+    # cv2.circle(leftImg,center,radius,(0,255,0),2)
+    # cv2.circle(rightImg,center2,radius2,(0,255,0),2)
+    # calculate depth
     #     f=10
     #     T=8
     #     k=10000
@@ -73,12 +77,13 @@ def calculate_depth(center1, center2, radius, radius2):
     #     if True:
             # print depth
     br = tf.TransformBroadcaster()
-    br.sendTransform((1.0*x/20, 1.0*depth/20, 1.0*z/20),
+    br.sendTransform((1.0 * x / 20, 1.0 * depth / 20, 1.0 * z / 20),
                      tf.transformations.quaternion_from_euler(0, 0, 0),
                      rospy.Time.now(),
                      "ball",
                      "base1")
     return (x, depth, z)
+
 
 def bgr_to_center_radius(im_bgr,
                          hsv_lows=hsv_lows_default,
@@ -139,6 +144,7 @@ def plot_center_radius(im, centers, radiuses):
             cv2.circle(im, center, radius, (0, 255, 0), 2)
     return im
 
+
 def plot_center_radius2(im2, centers2, radiuses2):
     """
     Plot circles of centers and radius to im
@@ -157,33 +163,34 @@ if __name__ == '__main__':
     # main loop
     while(True):
         if (not rightReady) or (not leftReady):
-        # rospy.spin()
+            # rospy.spin()
             continue
         # read frame
         # (_, im_bgr) = camera.read()
         # print type(rightImg)
         try:
-            im_bgr = rightImg#cv2.cv.QueryFrame(rightImg)
-            im_bgr = cv2.cvtColor( im_bgr, cv2.COLOR_BGR2RGB)
+            im_bgr = rightImg  # cv2.cv.QueryFrame(rightImg)
+            im_bgr = cv2.cvtColor(im_bgr, cv2.COLOR_BGR2RGB)
             im_bgr_left = cv2.cvtColor(leftImg, cv2.COLOR_BGR2RGB)
-        # get centers and radiuses
+            # get centers and radiuses
             centers, radiuses = bgr_to_center_radius(im_bgr)
             centers_left, radiuses_left = bgr_to_center_radius(im_bgr_left)
 
-        # plot center and radius
+            # plot center and radius
             im_bgr = plot_center_radius(im_bgr, centers, radiuses)
             im_bgr_left = plot_center_radius2(im_bgr_left, centers_left, radiuses_left)
-        # print out the depth
+            # print out the depth
             # try:
             if len(centers) > 0 and len(centers_left) > 0:
                 print centers[0]
                 print centers_left[0]
-                calculate_depth(centers[0], centers_left[0], radiuses[0], radiuses_left[0])
+                calculate_depth(
+                    centers[0], centers_left[0], radiuses[0], radiuses_left[0])
             # except IndexError:
                 # a=1
             # print centers
             # print centers_left
-        # display the resulting frame
+            # display the resulting frame
             cv2.imshow('frame', im_bgr)
             cv2.imshow('frame2', im_bgr_left)
             # print centers
