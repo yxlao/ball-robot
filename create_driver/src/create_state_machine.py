51ae2c617373586af565a6c4c1b0afb5f7fb8686
Douglas Chan <dtchan@eng.ucsd.edu>
2016-02-24 15:11:24 -0800

    state machine changes

--- create_driver/src/create_state_machine.py
+++ create_driver/src/create_state_machine.py
@@ -72,6 +72,7 @@ def stop():
 
 
 def ir_bumper_callback(msg):
+    global done_avoiding_obstacle
     if msg.state and done_avoiding_obstacle:
         state = "avoid"
         done_avoiding_obstacle = False
