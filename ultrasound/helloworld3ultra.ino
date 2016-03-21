7d80cba4ddeb51578a963992a786ead5eb8e1681
Yixing Lao <linuxthink@gmail.com>
2016-03-20 17:48:54 -0700

    reformat and restructure ultrasounds

--- ultrasound/helloworld3ultra.ino
+++ ultrasound/helloworld3ultra.ino
@@ -22,9 +22,9 @@ NewPing sonar(12, 11, 300); // NewPing setup of pins and maximum distance.
 NewPing front_sonar(10, 8, 300);
 
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 // Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
@@ -47,22 +47,22 @@ ros::Publisher front_ultrasonic_pub("front_ultrasonic", &ultrasonic_msg);
 char forward[4] = "fwd";
 char back[3] = "bk";
 unsigned int ultrasonic_dist;
-int MOTOR_SPEED= 90;
+int MOTOR_SPEED = 90;
 
-void callback(const std_msgs::String& msg){
+void callback(const std_msgs::String& msg) {
   uint8_t i;
   String msgdata(msg.data);
 //  String w("w");
 //  String s("s");
   arm_cmd.data = msg.data;
-  if (strcmp(msg.data, "e") == 0){
+  if (strcmp(msg.data, "e") == 0) {
     arm_cmd.data = forward;
     ////run the second motor
     myMotor2->run(FORWARD);
     myMotor2->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle > a){
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -73,22 +73,22 @@ void callback(const std_msgs::String& msg){
     myMotor2->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle < b) {
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
 //    myMotor2->run(RELEASE);
 //    delay(50);
-  } 
+  }
 //  myMotor2->run(RELEASE);
-   if (strcmp(msg.data, "w") == 0){
+  if (strcmp(msg.data, "w") == 0) {
     arm_cmd.data = forward;
     ////run the second motor
     myMotor->run(FORWARD);
     myMotor->setSpeed(150);
 //    delay(500);
 //    while (angle > a){
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -99,7 +99,7 @@ void callback(const std_msgs::String& msg){
     myMotor->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle < b) {
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -110,20 +110,20 @@ void callback(const std_msgs::String& msg){
     myMotor4->setSpeed(100);
     delay(1500);
     myMotor4->run(RELEASE);
-  }else if (strcmp(msg.data, "c") == 0) {
+  } else if (strcmp(msg.data, "c") == 0) {
     myMotor4->run(FORWARD);
     myMotor4->setSpeed(100);
     delay(1500);
     myMotor4->run(RELEASE);
   }
-  if (strcmp(msg.data, "f") == 0){
+  if (strcmp(msg.data, "f") == 0) {
     arm_cmd.data = forward;
     ////run the second motor
     myMotor3->run(FORWARD);
     myMotor3->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle > a){
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -134,7 +134,7 @@ void callback(const std_msgs::String& msg){
     myMotor3->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle < b) {
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -142,14 +142,14 @@ void callback(const std_msgs::String& msg){
 //    delay(50);
   }
 
-   if (strcmp(msg.data, "t") == 0){
+  if (strcmp(msg.data, "t") == 0) {
     arm_cmd.data = forward;
     ////run the second motor
     myMotor4->run(FORWARD);
     myMotor4->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle > a){
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -160,7 +160,7 @@ void callback(const std_msgs::String& msg){
     myMotor4->setSpeed(MOTOR_SPEED);
 //    delay(500);
 //    while (angle < b) {
-////      chatter.publish("%4.2f", angle); 
+////      chatter.publish("%4.2f", angle);
 //      nh.spinOnce();
 //      delay(5);
 //    }
@@ -173,30 +173,29 @@ void callback(const std_msgs::String& msg){
     myMotor2->run(RELEASE);
     myMotor3->run(RELEASE);
     myMotor4->run(RELEASE);
-    
+
   }
-  
+
 
   chatter.publish(&arm_cmd);
   delay(50);
-  
+
 }
 
 ros::Subscriber<std_msgs::String> cmds("arm_cmds", &callback);
-void setup()
-{
+void setup() {
   nh.initNode();
   nh.advertise(chatter);
   nh.advertise(back_ultrasonic_pub);
   nh.advertise(front_ultrasonic_pub);
 //  nh.advertise(ard_angle_pub);
   nh.subscribe(cmds);
-    Serial.begin(57600);           // set up Serial library at 9600 bps
+  Serial.begin(57600);           // set up Serial library at 9600 bps
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  
+
   // Set the speed to start, from 0 (off) to 255 (max speed)
   myMotor->setSpeed(20);
   myMotor2->setSpeed(20);
@@ -208,7 +207,7 @@ void setup()
   myMotor2->run(FORWARD);
   // turn on motor
   myMotor2->run(RELEASE);
-    myMotor3->run(FORWARD);
+  myMotor3->run(FORWARD);
   // turn on motor
   myMotor3->run(RELEASE);
   myMotor4->run(FORWARD);
@@ -216,14 +215,13 @@ void setup()
   myMotor4->run(RELEASE);
 }
 
-void loop()
-{
+void loop() {
 //  str_msg.data = to_string(angle);
 //  chatter.publish( &str_msg );
   ultrasonic_dist = sonar.ping_cm();
   ultrasonic_msg.data = ultrasonic_dist;
   back_ultrasonic_pub.publish(&ultrasonic_msg);
-  
+
   ultrasonic_dist = front_sonar.ping_cm();
   ultrasonic_msg.data = ultrasonic_dist;
   front_ultrasonic_pub.publish(&ultrasonic_msg);
--- ultrasound/ultrasound.ino
+++ ultrasound/ultrasound.ino
@@ -8,16 +8,19 @@
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN,
+              MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void setup() {
-  Serial.begin(57600); // Open serial monitor at 115200 baud to see ping results.
+    Serial.begin(57600); // Open serial monitor at 115200 baud to see ping results.
 }
 
 void loop() {
-  delay(500);                      // Wait 500ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-  Serial.print("Ping: ");
-  Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
-  Serial.println("cm");
+    delay(500);                      // Wait 500ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+    unsigned int uS =
+        sonar.ping(); // Send ping, get ping time in microseconds (uS).
+    Serial.print("Ping: ");
+    Serial.print(uS /
+                 US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+    Serial.println("cm");
 }
